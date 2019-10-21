aacb6cd598a6b114418189f68c7e4b096cbffcc0
Александр Косов <mos.cap14@yandex.ru>
2019-10-21 18:08:39 +0300

    Border relations were added

--- classic_segmentation.py
+++ classic_segmentation.py
@@ -8,6 +8,7 @@ from scipy import ndimage as nd
 import time
 import math
 import skimage.measure as skm
+from skimage.util import invert
 import copy
 
 Calc_g = nx.DiGraph()
@@ -122,42 +123,20 @@ def init_dilate_kernel(size_x, size_y):
     dilate_kernel[-1, 0] = 0
     
     return dilate_kernel
-        
 
-def make_sp(image, rays, dots, radius):    
+def post_process(mask):
     fig, axes = plt.subplots(ncols=2, figsize=(20, 8), sharex=True, sharey=True)
     ax = axes.ravel()
     
-    initialize_graphs(rays, dots)      
-    mask = np.zeros([image.shape[0], image.shape[1]], dtype=np.uint16)
-    
-    ext_im = np.pad(image, (radius, radius), 'reflect')
-    ext_mask = np.pad(mask, (radius, radius), 'reflect')
-    frag_map = create_frag_map(radius, rays, dots)
-    
-    return image
-    
-    for i in range(0, image.shape[0], 10):
-        for j in range(0, image.shape[1], 10):
-            ext_mask = process_point(ext_im, ext_mask, frag_map, i, j, rays, dots, radius, 0.0)
-#            print("OK", j, i)
-#    ext_mask = process_point(ext_im, ext_mask, ext_sobel, frag_map, 70, 70, rays, dots, radius, 0.0)
-    mask = ext_mask[radius:-radius, radius:-radius]
-    
     kernel = np.ones((3,3))
     dilate_kernel = init_dilate_kernel(21,21)
-    smooth_kernel = init_dilate_kernel(7,7)
+    smooth_kernel = init_dilate_kernel(5,5)
     
     mask = np.double(mask)
     mask = mask / np.max(np.max(mask))
     mask[mask < 0.15] = 0
     mask = np.uint8(mask * 255)
     
-    ax[0].imshow(mask, cmap=plt.cm.gray, interpolation='nearest')
-    ax[0].set_title('Markers')
-    
-    
-#    mask3 = nd.grey_closing(mask3, (5,5))
     watershed = np.where(mask[:,:] < 0.1, 1, 0)
     watershed = nd.label(watershed)[0]
     watershed = np.uint16(nd.watershed_ift(mask, watershed))
@@ -176,12 +155,15 @@ def make_sp(image, rays, dots, radius):
             maxx = summ;
             
     for i in range(1, classes):
-        summ = sum(sum(np.where(watershed == i, 1, 0)))
-        if summ <= 0.3 * maxx:
+        segment = np.where(watershed == i, 1, 0)
+        summ = sum(segment[:, -1]) + sum(segment[:, 0]) + sum(segment[-1, :]) + sum(segment[0, :])
+        is_bordered = (summ) > 0 
+        summ = sum(sum(segment))
+        if summ <= (0.25 - (0.15 if is_bordered else 0)) * maxx:
             watershed[watershed == i] = 0
             
     watershed[watershed > 0] = 1        
-    watershed = nd.binary_dilation(watershed, structure = dilate_kernel)
+    watershed = nd.binary_dilation(watershed, structure = smooth_kernel)
     
     watershed, classes = nd.label(watershed)
     
@@ -190,9 +172,29 @@ def make_sp(image, rays, dots, radius):
         summ = sum(sum(segment_map))
         perim = skm.perimeter(segment_map, neighbourhood=8)
         
-        if (float(perim) / np.sqrt(summ)) > 12   :
+        if (float(perim) / np.sqrt(summ)) > 8   :
             watershed[watershed == i] = 0
+    return watershed
+        
+
+def make_sp(image, rays, dots, radius):      
+    initialize_graphs(rays, dots)      
+    mask = np.zeros([image.shape[0], image.shape[1]], dtype=np.uint16)
     
+    ext_im = np.pad(image, (radius, radius), 'reflect')
+    ext_mask = np.pad(mask, (radius, radius), 'reflect')
+    frag_map = create_frag_map(radius, rays, dots)
+    
+    
+    for i in range(0, image.shape[0], 10):
+        for j in range(0, image.shape[1], 10):
+            ext_mask = process_point(ext_im, ext_mask, frag_map, i, j, rays, dots, radius, 0.0)
+#            print("OK", j, i)
+#    ext_mask = process_point(ext_im, ext_mask, ext_sobel, frag_map, 70, 70, rays, dots, radius, 0.0)
+    mask = ext_mask[radius:-radius, radius:-radius]   
+    
+    
+    watershed = post_process(mask)
     
     new_im = cv.cvtColor(np.uint8(image), cv.COLOR_GRAY2BGR)
     
