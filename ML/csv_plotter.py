abb160487955a0754349e754dab4039a208c5f45
Александр Косов <mos.cap14@yandex.ru>
2020-04-02 14:25:27 +0300

    New command line arguments logic

--- ML/csv_plotter.py
+++ ML/csv_plotter.py
@@ -48,13 +48,32 @@ def csv_plotter(file_obj, plots, title='', save_path = None):
     ax.set_xlabel('Epoch', fontsize = 30)      
     fig.tight_layout() 
     
-if __name__ == "__main__":
+def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--file", required=True,
         help="path to csv file")
+    ap.add_argument("-c", "--code", required=True,
+        help="4-digit code to define plotting values\n" +
+        "neach digit sets visibility of corresponding value.\n" + 
+        "Vales are ACCURACY, LOSS, VAL_ACCYRACY, VAL_LOSS in same order.\n" + 
+        "1100 mens to plot ACCURACY and LOSS")
     ap.add_argument("-s", "--save", required=False,
         help="where to save file")
+    ap.add_argument("-t", "--title", required=False,
+        help="plot title")
     args = vars(ap.parse_args())
     
     with open(args["file"], "r") as f_obj:
-        csv_plotter(f_obj, [ACCURACY, LOSS])
+        code = int(args["code"])
+        metrics = [VAL_LOSS, VAL_ACC, LOSS, ACCURACY]
+        plots = []        
+        
+        for i in range(4):
+            if code % 10 == 1:
+                plots.append(metrics[i])
+            code //= 10   
+            
+        csv_plotter(f_obj, plots, args["title"], args["save"])
+    
+if __name__ == "__main__":
+    main()
