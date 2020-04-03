a460c41b56d9407ff00f82aa2bf269c0bb326b04
Александр Косов <mos.cap14@yandex.ru>
2020-04-03 21:58:21 +0300

    New image generator and nearly no magic

--- prepare_samples.py
+++ prepare_samples.py
@@ -16,28 +16,75 @@ import copy
 import os
 import imutils
 
+ROTATION_ANGLES = [0, 90, 180, 270]
+MIN_REGION_AREA_RATIO = 0.7
+REGION_AREA_TRESHOLD = 400
+RESIZE_SHAPE = (256, 256)
+RATIO_TRESHOLD = 3.0
+GENERATING_MAX_SIZE = 250
+GENERATING_MIN_SIZE = 1
+BIAS_PORTION = 0.1
+BIG_SHIFT_STEP = 3
+STEPS_TO_TRY = 10
+AUG_STEPS = 32
+
+AXIS_X = 1
+AXIS_Y = 0
+
 def bad_foo(image, mask, path, i, j): #finding "nongland" regions
     mask[mask > 0] = 1
     num = 0
     
     while num < j: #doing actions until desired number of "nongland" regions were riched
-        x = np.random.randint(0, image.shape[1] - 1) #generating random x,y,w,h
-        y = np.random.randint(0, image.shape[0] - 1)        
-        w = np.random.randint(1, 250)
-        h = np.random.randint(1, 250)
+        angle = np.random.randint(0, len(ROTATION_ANGLES))
+        angle = ROTATION_ANGLES[angle]
+        image = imutils.rotate_bound(image, angle)
+        mask = imutils.rotate_bound(mask, angle)
+    
+        x = np.random.randint(0, image.shape[AXIS_X] - 1) #generating random x,y,w,h
+        y = np.random.randint(0, image.shape[AXIS_Y] - 1)        
+        w = np.random.randint(GENERATING_MIN_SIZE, GENERATING_MAX_SIZE)
+        h = np.random.randint(GENERATING_MIN_SIZE, GENERATING_MAX_SIZE)
         
-        ny = max(1, min(image.shape[0] - 1, y + h)) #customizing to avoid index-is-out-of-range exception
-        nx = max(1, min(image.shape[1] - 1, x + w))
+        ny = max(1, min(image.shape[AXIS_Y] - 1, y + h)) #customizing to avoid index-is-out-of-range exception
+        nx = max(1, min(image.shape[AXIS_X] - 1, x + w))
         
         roi=np.where(mask[y:ny,x:nx] > 0, 0, 1) #mask for "nongland" area
         summ = np.sum(roi) #counting "nongland" area       
-        if (np.float(summ) / (w * h) > 0.7) and (w * h > 400) and (np.float(w) / h > 0.33) and (np.float(w) / h < 3):
-            cv.imwrite(path + "/" + str(i + num) + ".png", image[y:ny,x:nx]) #some cases to approve the region
+        if (
+                np.float(summ) / (w * h) > MIN_REGION_AREA_RATIO and 
+                w * h > REGION_AREA_TRESHOLD and 
+                np.float(w) / h > 1 / RATIO_TRESHOLD and
+                np.float(w) / h < RATIO_TRESHOLD
+            ):
+            patch = cv.resize(image[y:ny,x:nx], RESIZE_SHAPE, interpolation = cv.INTER_LINEAR)
+            cv.imwrite(path + "/" + str(i + num) + ".png", patch) #some cases to approve the region
             num = num + 1
+            
+def check_sample_with_bias(image, label, path, x, y, w, h, bx, by, bw, bh, i):    
+    y = max(0, min(image.shape[AXIS_Y] - 1, y + by)) #customizing to avoid index-is-out-of-range exception
+    x = max(0, min(image.shape[AXIS_X] - 1, x + bx))
+    w = max(1, w + bw)
+    h = max(1, h + bh)
+    ny = max(1, min(image.shape[AXIS_Y] - 1, y + h))
+    nx = max(1, min(image.shape[AXIS_X] - 1, x + w))
+                
+    roi=np.where(label[y:ny,x:nx] > 0, 1, 0) #mask for "nongland" area
+    summ = np.sum(roi) #counting "nongland" area  
+    if (
+            np.float(summ) / (w * h) > MIN_REGION_AREA_RATIO and 
+            w * h > REGION_AREA_TRESHOLD and 
+            np.float(w) / h > 1 / RATIO_TRESHOLD and
+            np.float(w) / h < RATIO_TRESHOLD
+        ):
+        patch = cv.resize(image[y:ny,x:nx], RESIZE_SHAPE, interpolation = cv.INTER_LINEAR)
+        cv.imwrite(path + "/" + str(i) + ".png", patch) #some cases to approve the region
+        i = i + 1
+    return i
         
     
 
-def segmentation_foo(image, mask, path, i): #constructing "gland" samples
+def segmentation_foo(image, mask, path, i): #constructing "gland" samples    
     mask[mask > 0] = 1
     label, classes = nd.label(mask) #separating "gland" regions
     
@@ -48,21 +95,32 @@ def segmentation_foo(image, mask, path, i): #constructing "gland" samples
         for cnt in contours:
             idx += 1
             x,y,w,h = cv.boundingRect(cnt) #making bounding rect
-            for k in range(5):
-                bx, by, bw, bh = np.random.randint(-15, 15, 4) #random modificaions
-                
-                y = max(0, min(image.shape[0] - 1, y + by)) #customizing to avoid index-is-out-of-range exception
-                x = max(0, min(image.shape[1] - 1, x + bx))
-                w = max(1, w + bw)
-                h = max(1, h + bh)
-                ny = max(1, min(image.shape[0] - 1, y + h))
-                nx = max(1, min(image.shape[1] - 1, x + w))
-                
-                roi=np.where(label[y:ny,x:nx] > 0, 1, 0) #mask for "nongland" area
-                summ = np.sum(roi) #counting "nongland" area  
-                if (np.float(summ) / (w * h) > 0.7) and (w * h > 400) and (np.float(w) / h > 0.33) and (np.float(w) / h < 3):
-                    cv.imwrite(path + "/" + str(i) + ".png", image[y:ny,x:nx]) #some cases to approve the region
-                    i = i + 1
+            
+            bias = np.floor(BIAS_PORTION * max(w, h))
+            if bias == 0:
+                continue
+            
+            bx, by, bw, bh = np.random.randint(-bias, bias, 4) #random modificaions                
+            i = check_sample_with_bias(image, label, path, x, y, w, h, bx, by, bw, bh, i)                
+            
+            if i % BIG_SHIFT_STEP == 0:
+                k = copy.deepcopy(i)      
+                steps = 0
+                while k == i and steps < STEPS_TO_TRY:
+                    small_bias = max(bias // 3, 1)
+                    bx, by = np.random.randint(-small_bias, small_bias, 2) #random modificaions for big shift
+                    
+                    if w > h:
+                        bw = np.random.randint(-6 * bias, -4 * bias)
+                        bh = np.random.randint(-small_bias, 0)
+                    else:
+                        bh = np.random.randint(-6 * bias, -4 * bias)
+                        bw = np.random.randint(-small_bias, 0)
+                        
+                    k = check_sample_with_bias(image, label, path, x, y, w, h, bx, by, bw, bh, i)
+                    steps = steps + 1 
+                    
+                i = copy.deepcopy(k)            
     return i
 
 
@@ -82,7 +140,7 @@ def main():
     imagePaths = sorted(list(paths.list_images(args["images"]))) #reading images and annotations paths 
     maskPaths = sorted(list(paths.list_images(args["masks"])))
     
-    i = np.int(0)
+    i = 0
     for imagePath, maskPath in zip(imagePaths, maskPaths):
         print(imagePath + " - " + maskPath)
         img = cv.imread(imagePath)
@@ -90,17 +148,24 @@ def main():
         
         mask = cv.imread(maskPath)
         mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
-        for k in range(4): # "flip" augmentations
-            if k / 2 == 1:
+        for k in range(AUG_STEPS): # "flip" augmentations     
+            angle = np.random.randint(0, len(ROTATION_ANGLES))
+            angle = ROTATION_ANGLES[angle]
+            img = imutils.rotate_bound(img, angle)
+            mask = imutils.rotate_bound(mask, angle)
+        
+            if (k // 2) % 2 == 1:
                 cv.flip(img, 0)
                 cv.flip(mask, 0)
                 
             if k % 2 == 1:
                 cv.flip(img, 1)
                 cv.flip(mask, 1)
+                
             j = segmentation_foo(img, mask, args["glands"], i) #generating "gland" regions
             bad_foo(img, mask, args["nonglands"], i, j - i) #and "nongland" regions here
             i = copy.deepcopy(j)
+            
     print(i)
 
 if __name__ == "__main__":
